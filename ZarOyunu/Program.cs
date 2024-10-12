61eda45c87e0bec1280ee03feb969586d8f3c5a0
Sumeyye Buran <sumeyye.b0123@gmail.com>
2024-10-12 17:41:58 +0300

    c# game

---
+++ ZarOyunu/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ZarOyunu 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TavlaZari tavlaZari1 = new TavlaZari(8);
+            TavlaZari tavlaZari2 = new TavlaZari(8);
+
+            int player1 = tavlaZari1.ZarAt();
+            int player2 = tavlaZari1.ZarAt();
+
+            Console.WriteLine("1. oyuncunun attığı zar: " + player1);
+            Console.WriteLine("2. oyuncunun attığı zar: " + player2);
+
+            Console.WriteLine();
+
+            if (player1 > player2)
+            {
+                Console.WriteLine("1. oyuncu kazandı");
+            }
+            else
+            {
+                Console.WriteLine("2. oyuncu kazandı");
+            }
+        }
+    }
+}
---
+++ ZarOyunu/TavlaZari.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZarOyunu
+{
+    class TavlaZari
+    {
+        /// <summary>
+        /// Zarin alabileceği maksimum değer degiskeni
+        /// </summary>
+        int makZarDegeri;
+
+        //rastgele sayı uretme
+        Random rnd = new Random();
+
+        /// <summary>
+        /// Zarin alabilecegi maksimum değeri giriniz
+        /// </summary>
+        /// <param name="makZarDegeri"></param>
+        //constructor
+        public TavlaZari(int makZarDegeri)
+        {
+            this.makZarDegeri = makZarDegeri; // gelen degiskenle class daki degiskeni ayırır
+        }
+
+        /// <summary>
+        /// Rastgale bir sayı icin zar at
+        /// </summary>
+        /// <returns></returns>
+        public int ZarAt()
+        {
+            // 1 ile 8+1 arasında sayı üretecek,yani 1,2,3,4,5,6,7,8
+            return rnd.Next(1,makZarDegeri+1);
+        }
+
+    }
+}
